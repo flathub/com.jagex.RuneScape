9b1834015dc197c9a04e617594ad95b33ce0aa6c
Jonathan Garbee <jonathan@garbee.me>
2018-07-25 15:38:05 -0400

    chore: add xdotool
    Include xdotool as a required dependency.
    
    Update launcher script to hide the launcher window automatically once the game is loaded.
    If the user runs with the SHOW_LAUNCHER environment variable as non-empty, it won't hide the launcher.

--- scripts/runescape.sh
+++ scripts/runescape.sh
@@ -4,4 +4,19 @@
 # This is to fix crackling audio issues when running inside of the container.
 [ '$PULSE_LATENCY_MSEC' ] || export PULSE_LATENCY_MSEC='80'
 
-exec /app/extra/runescape-launcher "$@"
+exec /app/extra/runescape-launcher "$@" &
+
+if [ -z "${SHOW_LAUNCHER}" ]; then
+    # The game won't be up instantly. So wait until we see 2 visible RuneScape windows.
+    # While we're waiting, grab the first window ID. This will be the launcher window.
+    until [ $(xdotool search --all --onlyvisible --classname "RuneScape" | wc -l) -eq 2 ]; do
+        [ "$launcherId" ] || launcherId=$(xdotool search --all --onlyvisible --classname "RuneScape")
+        sleep 2
+    done
+
+    # Unmap the launcher window from X. This keeps it running, but hides it from view.
+    # It will close automatically when the game is closed by the user.
+    xdotool windowunmap "$launcherId"
+fi
+
+exit 0
