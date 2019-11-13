99d6556b8fa46b9a66ccd3f0cb9205fd128eb555
Marc Boocha <marcboocha@gmail.com>
2019-11-13 20:10:39 +0800

    Trailing spaces removed
    Signed-off-by: Marc Boocha <marcboocha@gmail.com>
    
     Changes to be committed:
    	modified:   scripts/runescape.sh

--- scripts/runescape.sh
+++ scripts/runescape.sh
@@ -3,8 +3,7 @@
 # Use the user provided pulseaudio latency or 80ms if unprovided.
 # This is to fix crackling audio issues when running inside of the container.
 PULSE_LATENCY_MSEC=${PULSE_LATENCY_MSEC:-80}
-
 export PULSE_LATENCY_MSEC
 
- exec /app/extra/runescape-launcher "$@"
+exec /app/extra/runescape-launcher "$@"
 
