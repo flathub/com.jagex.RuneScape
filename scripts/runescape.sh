70306d4dd10faca0370f4b410b49f217d855ddf4
Marc Boocha <marcboocha@gmail.com>
2019-11-13 19:42:23 +0800

    Updated Binary to version 2.2.5
    Signed-off-by: Marc Boocha <marcboocha@gmail.com>
    
     Changes to be committed:
    	modified:   bin/cron
    	modified:   bin/debian-update-check.sh
    	modified:   com.jagex.RuneScape.json
    	deleted:    glew-no-static-lib.patch
    	modified:   scripts/runescape.sh

--- scripts/runescape.sh
+++ scripts/runescape.sh
@@ -2,21 +2,9 @@
 
 # Use the user provided pulseaudio latency or 80ms if unprovided.
 # This is to fix crackling audio issues when running inside of the container.
-[ '$PULSE_LATENCY_MSEC' ] || export PULSE_LATENCY_MSEC='80'
+PULSE_LATENCY_MSEC=${PULSE_LATENCY_MSEC:-80}
 
-exec /app/extra/runescape-launcher "$@" &
+export PULSE_LATENCY_MSEC
 
-if [ -z "${SHOW_LAUNCHER}" ]; then
-    # The game won't be up instantly. So wait until we see 2 visible RuneScape windows.
-    # While we're waiting, grab the first window ID. This will be the launcher window.
-    until [ $(xdotool search --all --onlyvisible --classname "RuneScape" | wc -l) -eq 2 ]; do
-        [ "$launcherId" ] || launcherId=$(xdotool search --all --onlyvisible --classname "RuneScape")
-        sleep 2
-    done
+ exec /app/extra/runescape-launcher "$@"
 
-    # Unmap the launcher window from X. This keeps it running, but hides it from view.
-    # It will close automatically when the game is closed by the user.
-    xdotool windowunmap "$launcherId"
-fi
-
-exit 0
